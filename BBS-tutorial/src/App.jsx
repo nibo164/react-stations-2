f6cb555399c52617691466b1e500f6873ee47e84
nibo164 <warimiga164@gmail.com>
2025-04-22 11:32:07 +0900

    [update]ThreadListコンポーネントを追加

--- BBS-tutorial/src/App.jsx
+++ BBS-tutorial/src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./Header.jsx";
+import ThreadList from "./ThreadList.jsx";
 import "./App.css";
 const App = () => {
   return (
     <>
       <Header />
+      <ThreadList />
     </>
   );
 };
