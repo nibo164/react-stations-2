e503cb56dbe4c82a2ec8d8470c5c005884d6772e
nibo164 <warimiga164@gmail.com>
2025-06-04 18:53:08 +0900

    [update]Headerコンポーネント内にMakeThreadButtonコンポーネントを追加

--- BBS-tutorial/src/Header.jsx
+++ BBS-tutorial/src/Header.jsx
@@ -1,8 +1,11 @@
+import MakeThreadButton from "./MakeThreadButton.jsx";
+
 const Header = () => {
   return (
     <>
       <header className="header">
         <h1>Nibo's BBS</h1>
+        <MakeThreadButton />
       </header>
     </>
   );
