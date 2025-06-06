a2e0565bd8d6ed318d06a1232396fa5bb8c281db
nibo164 <warimiga164@gmail.com>
2025-06-06 17:34:38 +0900

    [add]MakeThreadMenuコンポーネントを作成

---
+++ BBS-tutorial/src/MakeThreadMenu.jsx
@@ -0,0 +1,12 @@
+const MakeThreadMenu = () => {
+  return (
+    <>
+      <h2>スレッド新規作成</h2>
+      <form action="" method="post">
+        <input type="text" placeholder="スレッドタイトル" />
+      </form>
+    </>
+  );
+};
+
+export default MakeThreadMenu;
