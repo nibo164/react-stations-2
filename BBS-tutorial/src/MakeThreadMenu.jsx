fe079d9199f5d454c5efe7dffd63a53e1faa137a
nibo164 <warimiga164@gmail.com>
2025-06-06 18:30:16 +0900

    [update]MakeThreadMenu.jsxにAPIへのpost機能を追加

--- BBS-tutorial/src/MakeThreadMenu.jsx
+++ BBS-tutorial/src/MakeThreadMenu.jsx
@@ -1,9 +1,27 @@
 const MakeThreadMenu = () => {
+  const handleSubmit = (formData) => {
+    fetch("https://railway.bulletinboard.techtrain.dev/threads", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ title: formData.get("title") }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("success:", data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+
   return (
     <>
       <h2>スレッド新規作成</h2>
-      <form action="" method="post">
-        <input type="text" placeholder="スレッドタイトル" />
+      <form action={handleSubmit} method="post">
+        <input type="text" name="title" placeholder="スレッドタイトル" />
+        <button type="submit">作成</button>
       </form>
     </>
   );
