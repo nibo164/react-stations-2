14e8b8ebb00f9369a7c6d1cb09a6ecb509b9da6e
nibo164 <warimiga164@gmail.com>
2025-06-06 17:33:44 +0900

    [update]App.jsxにスレッド作成画面のルートを追加

--- BBS-tutorial/src/App.jsx
+++ BBS-tutorial/src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route, Link, BrowserRouter } from "react-router-dom";
 import Header from "./Header.jsx";
 import ThreadList from "./ThreadList.jsx";
+import MakeThreadMenu from "./MakeThreadMenu.jsx";
 import "./App.css";
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Header />
       <Routes>
         <Route path="/" element={<ThreadList />} />
+        <Route path="/threads/new" element={<MakeThreadMenu />} />
       </Routes>
     </BrowserRouter>
   );
