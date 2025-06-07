e139144e0a5705ed710134e8aec2757b58fda411
nibo164 <warimiga164@gmail.com>
2025-06-08 01:41:38 +0900

    [update]投稿一覧画面へのルーティングを追加

--- BBS-tutorial/src/App.jsx
+++ BBS-tutorial/src/App.jsx
@@ -1,7 +1,8 @@
-import { Routes, Route, Link, BrowserRouter } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Header from "./Header.jsx";
 import ThreadList from "./ThreadList.jsx";
 import MakeThreadMenu from "./MakeThreadMenu.jsx";
+import Thread from "./Thread.jsx";
 import "./App.css";
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<ThreadList />} />
         <Route path="/threads/new" element={<MakeThreadMenu />} />
+        <Route path="/threads/:thread_id" element={<Thread />} />
       </Routes>
     </BrowserRouter>
   );
