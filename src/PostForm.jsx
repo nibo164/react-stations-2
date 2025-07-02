0eeac6f5d1b98aa65152513daa352a61e68b634f
nibo164 <warimiga164@gmail.com>
2025-07-02 17:44:16 +0900

    [update]投稿フォームのスタイル調整

--- src/PostForm.jsx
+++ src/PostForm.jsx
@@ -27,8 +27,18 @@ const PostForm = (props) => {
   return (
     <>
       <form onSubmit={handleSubmit}>
-        <input type="text" name="newPost" placeholder="投稿しよう！" />
-        <button type="submit">投稿</button>
+        <input
+          type="text"
+          name="newPost"
+          placeholder="投稿しよう！"
+          class="bg-white rounded-lg p-2 m-2 shadow-md focus:outline-none focus:ring-2 focus:ring-orange-500"
+        />
+        <button
+          type="submit"
+          class="text-lg text-white bg-orange-500 hover:bg-orange-600 p-2 m-2 rounded-lg"
+        >
+          投稿
+        </button>
       </form>
     </>
   );
